bb21be9f0afcdb065a0f2a3f996a03c13480c6c1
Shennawy <m.alshenaawy@mu.edu.sa>
2024-01-27 21:14:27 +0300

    FEATURE: get jira issue data and update commit link

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import {greetings} from "./misc/greetings.js";
 import add from "./git/add.js";
-import {commitLink} from "./git/commit.js";
+import commit, {commitLink} from "./git/commit.js";
 import sentence from "./questions/commitSentence.js";
 import chalk from "chalk";
 import push from "./git/push.js";
@@ -13,15 +13,15 @@ import questions from "./jira/questions.js";
 greetings();
 
 const addFiles = await files();
-const commit = await sentence();
-const commitSentence = `${commit.type}: ${commit.sentence}` + (commit.issueId ? ` ❯ ${commit.issueId}` : '');
+const commitData = await sentence();
+const commitSentence = `${commitData.type}: ${commitData.sentence}` + (commitData.issueId ? ` ❯ ${commitData.issueId}` : '');
 
 console.log(`\n [ Your commit => ${chalk.green(commitSentence)} ]\n`)
 
-// await add.command(addFiles)
-// await commit.command(commitSentence)
-// await pull.command();
-// await push.command();
+await add.command(addFiles)
+await commit.command(commitSentence)
+await pull.command();
+await push.command();
 
 const jiraCredential = readSettings('jira')
 if (commit.issueId && jiraCredential.verified)
